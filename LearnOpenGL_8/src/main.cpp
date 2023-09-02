2406d47972a8324453ff6e49f8a7f14c1c3bb668
ForgeCor3 <forgecor3@gmail.com>
2023-09-02 21:12:33 +0200

    LearnOpenGL_8.17 [GLM]

--- LearnOpenGL_8/src/main.cpp
+++ LearnOpenGL_8/src/main.cpp
@@ -164,6 +164,12 @@ int main()
         glClearColor(0.0, 0.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
